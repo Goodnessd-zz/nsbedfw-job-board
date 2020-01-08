c069b280b52f3b890492a2519132c1f9bfbeea0a
Goodness Daramola <gdaramol@thoughtworks.com>
2020-01-08 11:52:17 -0800

    Adding Spec| Goodness

---
+++ src/JobBoard.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import JobBoard from './JobBoard';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<JobBoard />, div);  
+});
