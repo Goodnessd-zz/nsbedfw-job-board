062281ff209d2f2bea08b29dd7a57a24c7b0d7c1
Goodness Daramola <gdaramol@thoughtworks.com>
2020-01-10 13:01:14 -0800

    adding real data|Goodness

--- src/Components/JobPost/JobPost.js
+++ src/Components/JobPost/JobPost.js
@@ -1,28 +1,40 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import JobBoard from '../JobBoard/JobBoard'
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345
+  },
+}));
 
 const JobPost = ({jobPost}) => {
   return (
     <div className="job-post">
       {jobPost ? (
-        <Card style={{margin:"10px auto"}}>
-          {/* <CardMedia style={{height: 0, paddingTop: '56.25%'}} */}
-          {/* title={jobPost.title}/> */}
+        <Card variant="outlined" style={{margin:"10px auto"}}>
           <CardContent>
             <Typography gutterBottom variant="headline" component="h2">
-              {jobPost.title}
+              Industry: {jobPost.industry}
             </Typography>
+            <Typography gutterBottom variant="headline" component="h3">
+              {jobPost.skillLevel} level position
+            </Typography>
+            {jobPost.companies_of_interest.length >0 ? (
+            <Typography gutterBottom variant="headline" component="h4">
+              Companies of Interest: {jobPost.companies_of_interest}
+            </Typography>):null}
             <Typography component ="p">
               {jobPost.description}
             </Typography>
           </CardContent>
-          <CardActions>
-            <Button size="small" color="primary" href={jobPost.url} target="_blank">
+          <CardActions >
+            <Button style ={{mariginLeft: "auto"}}size="small" color="primary" href={`mailto:${jobPost.email}`} target="_blank">
               Contact
             </Button>
           </CardActions>
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,70 +1,87 @@
 export const fetchJobPostings = () => {
 return [{
     "id": "1000",
-    "industry": "software",
-    "title": "Entry Level Software",
-    "description": "Looking for entry level software engineering job"
-  }, 
-  {
-    "id":"1001",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1002",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1003",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1004",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1005",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1006",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1007",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1008",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1009",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
-  }, 
-  {
-    "id":"1010",
-    "industry": "mechanical",
-    "title": "Entry Level Mechanical",
-    "description": "looking for entry level mechanical engineering job"
+    "industry": "Telecommunication",
+    "skillLevel": "Entry",
+    "companies_of_interest" : [],
+    "email": "kijyanasm@gmail.com"
   },
-
+  {
+    "id": "1001",
+    "industry": "Software",
+    "skillLevel": "Entry",
+    "companies_of_interest" : [],
+    "email": "cejiofor@gmail.com"
+  },
+  {
+    "id": "1002",
+    "industry": "General Technology",
+    "skillLevel": "Associate",
+    "companies_of_interest" : ["Ernst & Young, Delloite, IBM, Google"],
+    "email": "gnwaefulu@gmail.com"
+  },
+  {
+    "id": "1003",
+    "industry": "General Business",
+    "skillLevel": "Associate",
+    "companies_of_interest" : [],
+    "email": "charltonnwaefulu54@gmail.com"
+  },
+  {
+    "id": "1004",
+    "industry": "Business",
+    "skillLevel": "Entry",
+    "companies_of_interest" : ["AT&T, Microsoft, Verizon, Amazon, JP Morgan"],
+    "email": "jamesbrown5214@gmail.com"
+  },
+  {
+    "id": "1005",
+    "industry": "Software",
+    "skillLevel": "Associate",
+    "companies_of_interest" : ["Microsoft, Google, Apple, Amazon, Verizon, Deloitte"],
+    "email": "jamesbrown5214@gmail.com"
+  },
+  {
+    "id": "1006",
+    "industry": "Software",
+    "skillLevel": "Associate",
+    "companies_of_interest" : ["Microsoft, Google, Apple, Amazon, Verizon, Deloitte"],
+    "email": "eirivbogbe@gmail.com"
+  },
+  {
+    "id": "1007",
+    "industry": "Software",
+    "skillLevel": "Associate",
+    "companies_of_interest" : ["Microsoft, Deloitte, IBM, AT&T, Well Fargo, JP Morgan,Texas Instrument, Exxon"],
+    "email": "caholloman3@gmail.com"
+  },
+  {
+    "id": "1008",
+    "industry": "Other",
+    "skillLevel": "Entry",
+    "companies_of_interest" : ["Schlumberger"],
+    "email": "christopher.tolulope@yahoo.com"
+  },
+  {
+    "id": "1009",
+    "industry": "Software",
+    "skillLevel": "Entry",
+    "companies_of_interest" : [],
+    "email": "jhurta@gmail.com"
+  },
+  {
+    "id": "1010",
+    "industry": "Software",
+    "skillLevel": "Entry",
+    "companies_of_interest" : [],
+    "email": "omenkeukwu1@gmail.com"
+  },
+  {
+    "id": "1011",
+    "industry": "Cyber Security",
+    "skillLevel": "Associate",
+    "companies_of_interest" : ["Microsoft, Google, IBM"],
+    "email": "grace.fejokwu@gmail.com"
+  }
 ];
 };
